e749bb9d4135bc21504472a5bed18c1f13aa23dc
Filip Rec <filiprec@outlook.com>
2015-11-15 15:49:50 +0000

    Initial commit

---
+++ scannerapp/config.py
@@ -0,0 +1,12 @@
+"""
+Settings file for scanner.py
+"""
+
+keep_file = False
+# temp_dir = "/home/pi/scannertemp"
+temp_dir = "/var/www/scannerapp/scannerapp/static/img/temp"
+
+# some defaults maybe?
+
+#dpi = 300
+#format = tiff
