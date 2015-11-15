a4daaddb7cbad7ef708ef445148898d44df91692
Filip Rec <filiprec@outlook.com>
2015-11-15 17:56:30 +0100

    testing pyinsane

--- temp/scanner_detection.py
+++ temp/scanner_detection.py
@@ -6,3 +6,16 @@ device = devices[0]
 
 print("I'm going to use the following scanner: %s" % (str(device)))
 scanner_id = device.name
+
+device.options['resolution'].value = 300
+# Beware: Some scanner have "Lineart" or "Gray" as default mode
+device.options['mode'].value = 'Color'
+scan_session = device.scan(multiple=False)
+try:
+    while True:
+        scan_session.scan.read()
+except EOFError:
+    pass
+image = scan_session.images[0]
+
+print(image)
