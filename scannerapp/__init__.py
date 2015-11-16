5bb44ce2f2b1951ec0e3024de0e83e8f340f62b4
Filip Rec <filiprec@outlook.com>
2015-11-16 02:39:54 +0100

    Forgot to turn debug off...

--- scannerapp/__init__.py
+++ scannerapp/__init__.py
@@ -81,6 +81,6 @@ def get_uncropped():
         return send_file(path_to_file, as_attachment=True, attachment_filename=filename)
 
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=8000, debug=True)
+        app.run(host='0.0.0.0', port=8000)
 
 
