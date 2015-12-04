81a92693c2c066b1391e8ad90fff224096a7fd07
Filip Rec <filiprec@outlook.com>
2015-12-04 21:28:24 +0000

    Improved structure, added necessary comments, some errors now utilize Flask 'flash' feature

--- scannerapp/__init__.py
+++ scannerapp/__init__.py
@@ -9,7 +9,7 @@ import re
 import pyinsane.abstract as pyinsane
 from pyinsane.rawapi import SaneException
 from PIL import Image
-from flask import Flask, render_template, request, flash, url_for, send_file, jsonify, abort, make_response
+from flask import Flask, render_template, request, flash, url_for, send_file, jsonify, abort, make_response, flash, redirect
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import _W, _H
 
@@ -18,6 +18,10 @@ import config
 app = Flask(__name__, static_url_path='')
 app.secret_key = 'some_secret'
 temp_dir = getattr(config, 'temp_dir', '/tmp')
+
+"""
+Scanning function handler. Takes pyinsane device object and settings as keyword arguments. Returns a tuple with image object and error, based on the outcome of the scan attempt. 
+"""
 
 
 def perform_scan(scanner, **kwargs):
@@ -47,10 +51,14 @@ def perform_scan(scanner, **kwargs):
         error = 'No scanner detected.'
         return None, error
 
+"""
+Places a scanned image into a PDF file. Takes temp_filename (image filename), and filename (output filename requested by the user). Returns generated PDF filename. 
+"""
+
 
 def create_pdf(temp_filename, filename):
 
-    c = canvas.Canvas(temp_dir + filename)
+    c = canvas.Canvas(temp_dir + filename)  # saved with requested filename - since it already has correct extension
     image = Image.open(temp_dir + temp_filename)
     width, height = c.drawInlineImage(
         image, 0, 0, width=_W, height=_H, preserveAspectRatio=True)
@@ -61,12 +69,16 @@ def create_pdf(temp_filename, filename):
     return filename
 
 
-def temp_name_handler(format):
+# that name though...
+def temp_name_dir_handler(format):
+
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+
     temp_filename = ''.join(random.SystemRandom().choice(
         string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
-    """
-    If PDF is the desired output extension, save temporarily as jpeg to allow display 
-    """
+
+    # If PDF is the desired output extension, save temporarily as jpeg to allow displaying the scanned image 
     if format != 'pdf':
         temp_filename = temp_filename + '.' + format
     elif format == 'pdf':
@@ -74,8 +86,10 @@ def temp_name_handler(format):
 
     return temp_filename
 
+
 """
 API endpoint for listing available devices. This returns a JSON object with all the devices that, in theory, 'scanimage -L' would recognize. If failed, check your permissions.
+http://frec.pl/blog/dynamically-populating-forms-based-on-user-input-w/
 """
 
 
@@ -113,21 +127,20 @@ def scan_view():
         device_choice = request.form['device']
 
         devices = pyinsane.get_devices()
-        scanner = [device for device in devices if device.name in device_choice][0]  # bleh, I hate the way pyinsane returns available devices
+        # I hate the way pyinsane returns available devices - this sadly worked the best for what I needed
+        scanner = [device for device in devices if device.name in device_choice][0]
 
         image, log = perform_scan(scanner, resolution=resolution, mode=mode)
 
-        if not os.path.exists(temp_dir):
-            os.makedirs(temp_dir)
-
-        temp_filename = temp_name_handler(format)
+        temp_filename = temp_name_dir_handler(format)
 
         if image:
             image.save(temp_dir + temp_filename, quality=100)
             return render_template('scan.html', image=temp_filename, format=format)
 
         else:
-            return render_template('scan_failed.html', error=log)
+            flash(log)
+            return redirect(url_for('index'))
 
 
 @app.route('/get_uncropped?<string:temp_filename>&<string:format>', methods=['POST'])
@@ -136,13 +149,19 @@ def get_uncropped(format, temp_filename):
     if request.method == 'POST':
         pattern = re.compile('[\W]+')
         filename = pattern.sub('', request.form['filename']) + "." + format
-
+        # 'temp_filename' is the name of the image file, with extension
+        # 'filename' is the requested output name
         if format == 'pdf':
+            # calls PDF-generating function
+            # create_pdf returns name of the generated PDF file, variable name is sort
+            # of misleading since the function actually already saves the file
+            # under the requested name, but this ultimately means less code...
             temp_filename = create_pdf(temp_filename, filename)
 
-        path_to_file = temp_dir + temp_filename
+        path_to_file = temp_dir + temp_filename  # direct path to image/pdf
 
         return send_file(path_to_file, as_attachment=True, attachment_filename=filename)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=False)
